27119a92173c72c10789d741f4aad6cb12149bbd
zagal109aa <149740446+zagal109aa@users.noreply.github.com>
2023-12-03 03:27:49 +0900

    33

--- slideshow.js
+++ slideshow.js
@@ -1,27 +1,15 @@
-var slides = document.querySelectorAll("#slides>img");
-var prev = document.getElementById("prev");
-var next = document.getElementById("next");
 
 var current=0;
 Show_Slide(current);
-prev.onclick=Prev_Slide;
-next.onclick=Next_Slide;
-
 function Show_Slide(n){
-    for (var i=0;i<slides.length;i++){
+    var slides = document.querySelectorAll("#slides>img");
+    for(let i=0;<slides.length;i++){
         slides[i].style.display="none";
     }
-    slides[n].style.display="block";
-}
-
-function Prev_Slide(){
-    if(current>0) current-=1;
-    else current=slides.length -1;
-    Show_Slide(current);
-}
-
-function Next_Slide(){
-    if(current<slide.length-1) current+=1;
-    else current=0;
-    Show_Slide(current);
+    current++;
+    if(current>slides.length){
+        current=1;
+    }
+    slides[current-1].style.display="block";
+    setTimeout(Show_Slide,3000);
 }
